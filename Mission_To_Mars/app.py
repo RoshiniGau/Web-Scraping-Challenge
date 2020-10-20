64e2bddb6fb839844cae8bc8b0f955518aa190e4
Roshini <roshini.12j@gmail.com>
2020-10-20 12:54:42 -0400

    Adding app.py code

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -1,42 +1,34 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_mars
 
-# Create an instance of Flask
+
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
-
+app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_data"
+mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    destination_data= mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars_data=mars_data)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    # Run the scrape function and save the results to a variable
-    weather = scrape_costa.scrape_info()
-    forecast={
-        "min_temp":weather["min_temp"],
-        "max_temp":weather["max_temp"]
-    }
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape()
+
     # Update the Mongo database using update and upsert=True
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, forecast, upsert=True)
-
-
-    # Redirect back to home page
-    return redirect("http://localhost:5000/", code=302)
-
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    return 'All done!'
 
 if __name__ == "__main__":
     app.run(debug=True)
